c3eca82f0c4fd965c202b62120dd9fe04c71695e
elder.han <lingnanlu@gmail.com>
2021-02-08 16:40:42 +0800

    dev

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,23 @@
 def call(def distDir, def files, def run) {
+
     println(distDir)
     println(files)
     println(run)
+
+    def transfer = new ArrayList()
+
+    for (int i; i < files.size(); i++) {
+
+        transfer.add( sshTransfer(cleanRemote: false, sourceFiles: files[i], remoteDirectory: distDir))
+
+    }
+
+    sshPublisher(
+            publishers: [
+                    sshPublisherDesc(
+                            configName: "${SERVER}",
+                            transfers: transfer
+                    )
+            ]
+    )
 }
