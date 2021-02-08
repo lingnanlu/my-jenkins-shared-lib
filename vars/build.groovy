6023310165eb75f687dc194a167c61ab8b2e0c4a
elder.han <lingnanlu@gmail.com>
2021-02-08 16:51:28 +0800

    dev

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,9 +1,10 @@
 def call(def workDir, def cmd) {
-    println "build begin"
+    println "${'-' * 20}build begin${'-' * 20}"
     println "workdir is ${workDir}"
     println "cmd is ${cmd}"
 
     for (i in 0..<cmd.length) {
+        println cmd[i]
         sh cmd[i]
     }
 }
--- vars/log.groovy
+++ vars/log.groovy
@@ -1,5 +1,5 @@
 def call(def params, def env) {
-    println "pipeline begin"
+    println "${'-' * 20}pipeline begin${'-' * 20}"
     println "branch is ${params.BRANCH}"
     println "env is ${params.ENV}"
     println "server is ${params.SERVER}"
