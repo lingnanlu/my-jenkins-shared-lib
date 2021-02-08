886d92f9989c18ae92accd2754024665fb2de8f6
elder.han <lingnanlu@gmail.com>
2021-02-08 16:53:12 +0800

    dev

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,9 +1,9 @@
-def call(def workDir, def cmd) {
+def call(String workDir, List cmd) {
     println "${'-' * 20}build begin${'-' * 20}"
     println "workdir is ${workDir}"
     println "cmd is ${cmd}"
 
-    for (i in 0..<cmd.length) {
+    for (i in 0 ..< cmd.size()) {
         println cmd[i]
         sh cmd[i]
     }
