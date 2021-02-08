9779e9eecc2bfbbfa4d4c18bd7e4b77082129679
elder.han <lingnanlu@gmail.com>
2021-02-08 17:06:07 +0800

    dev

--- vars/build.groovy
+++ vars/build.groovy
@@ -3,6 +3,8 @@ def call(String workDir, List<String> cmd) {
     println "workdir is ${workDir}"
     println "cmd is ${cmd}"
 
+    cd workDir
+
     for (int i = 0; i < cmd.size(); i++) {
         println cmd[i]
         sh cmd[i]
