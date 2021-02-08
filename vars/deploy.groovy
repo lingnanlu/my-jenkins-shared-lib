f81dcefbc256341d1c3b8457aa9cec0777b471bc
elder.han <lingnanlu@gmail.com>
2021-02-08 18:01:59 +0800

    dev

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -12,27 +12,27 @@ def call(String distDir, List<String> files, List<String> run, String server) {
 
 //    def transferClourse =
 
-    sshPublisher(
-            publishers: [
-                    sshPublisherDesc(
-                            configName: "${server}",
-                            transfers: [
-                                    sshTransfer(cleanRemote: false, sourceFiles: files[0], remoteDirectory: distDir),
-                                    sshTransfer(cleanRemote: false, sourceFiles: files[1], remoteDirectory: distDir)
-                            ]
-                    )
-            ]
-    )
-
 //    sshPublisher(
 //            publishers: [
 //                    sshPublisherDesc(
 //                            configName: "${server}",
 //                            transfers: [
-//                                    sshTransfer(cleanRemote: false, sourceFiles: 'target/*.jar', removePrefix: 'target', remoteDirectory: distDir),
-//                                    sshTransfer(cleanRemote: false, sourceFiles: 'deploy/*', remoteDirectory: distDir)
+//                                    sshTransfer(cleanRemote: false, sourceFiles: files[0], remoteDirectory: distDir),
+//                                    sshTransfer(cleanRemote: false, sourceFiles: files[1], remoteDirectory: distDir)
 //                            ]
 //                    )
 //            ]
 //    )
+
+    sshPublisher(
+            publishers: [
+                    sshPublisherDesc(
+                            configName: "${server}",
+                            transfers: [
+                                    sshTransfer(cleanRemote: false, sourceFiles: 'target/*.jar', removePrefix: 'target', remoteDirectory: distDir),
+                                    sshTransfer(cleanRemote: false, sourceFiles: 'deploy/*', remoteDirectory: distDir)
+                            ]
+                    )
+            ]
+    )
 }
