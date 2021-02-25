dc033f9e63d1604404615f33749e20cf5f1b1c48
elder.han <lingnanlu@gmail.com>
2021-02-25 17:55:59 +0800

    dev

--- vars/snCI.groovy
+++ vars/snCI.groovy
@@ -3,17 +3,17 @@ def call(String yamlName) {
     try {
         log(params, env)
 
-        def yaml = readYaml file: yamlName;
-
-        if (yaml.build) {
-            def buildConf = yaml.build
-            build(buildConf.workDir, buildConf.cmd)
-        }
-
-        if (yaml.deploy) {
-            def deployConf = yaml.deploy
-            deploy(deployConf.distDir, deployConf.files, deployConf.run, params.SERVER)
-        }
+//        def yaml = readYaml file: yamlName;
+//
+//        if (yaml.build) {
+//            def buildConf = yaml.build
+//            build(buildConf.workDir, buildConf.cmd)
+//        }
+//
+//        if (yaml.deploy) {
+//            def deployConf = yaml.deploy
+//            deploy(deployConf.distDir, deployConf.files, deployConf.run, params.SERVER)
+//        }
     } finally {
 //        clean()
     }
